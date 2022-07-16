ae5d6f3264bff9f01cd90175f126fc8fb2267b23
baechu67 <libdev67@gmail.com>
2022-07-16 20:56:47 +0900

    주석 추가

--- TPC38.java
+++ TPC38.java
@@ -7,8 +7,9 @@ import java.util.List;
 
 public class TPC38 {
     public static void main(String[] args) {
-        // BookDTO 타입의 객체만 저장할 수 있도록 제네릭 사용: Object[] ---> BookDTO[]
-        // 다른 타입의 객체를 저장할 수 없어져 조금 더 안정적으로 사용 가능함
+        // BookDTO 타입의 객체만 저장할 수 있도록 제네릭 사용할 수 있다: Object[] ---> BookDTO[]
+        // 실제로 ArrayList에 객체를 저장할 때 서로 다른 타입의 객체를 함께 저장하는 일은 매우 드물다.
+        // 따라서 제네릭을 사용하면 다른 타입의 객체를 저장할 수 없고 한 타입의 객체만 저장할 수 있으므로 조금 더 안정적으로 사용 가능하다.
         List<BookDTO> list = new ArrayList<>(1);
 
         // ArrayList 생성 시 1로 초기화하여도 객체를 더 넣을 수 있음(길이 제약 없음)
